f2b6df87c4b78774b4c5a9b5c362f1952ee24133
Adam Croissant <adamc41@uw.edu>
2013-12-03 02:24:29 -0800

    put all the nodes in the repo into node.h

--- node.h
+++ node.h
@@ -128,6 +128,95 @@ class funcdef_node : public AST_node {
 class block_node : public AST_node {
   public:
     block_node(AST_node* vdecl_l, AST_node* stmt_l) : AST_node(vdecl_l, stmt_l) {}
-}
+};
+
+class assign_node : public AST_node {
+ public:
+ assign_node(AST_node* left, AST_node* right) : AST_node(left, right) {}
+};
+
+class IntLiteral_node : public AST_node {
+ public:
+  const int literal;
+ IntLiteral_node(int i) : literal(i) , AST_node() {}
+};
+
+class stringliteral_node : public AST_node {
+ public:
+  const nstring_st literal;
+ stringliteral_node(nstring_st str) : literal(str) , AST_node() {
+  }
+};
+
+class module_node : public AST_node {
+ public:
+  AST_node* funcdecl_list;
+  module_node(AST_node* vardecl_list, AST_node* funcdef_list,
+              AST_node* funcdecl_list) : AST_node(vardecl_list, funcdef_list) {
+    this->funcdecl_list = funcdecl_list;
+  }
+
+  ~() {
+    delete funcdecl_list;
+  }
+};
+
+class return_node : AST_node {
+ public:
+  // initializes left to point to the expression to return
+ return_node(AST_node *n1, AST_node *n2) : AST_node(n1, n2) {
+  }
+};
+
+class ternary_node : AST_node {
+ public:
+  AST_node* question;
+  
+ ternary_node(AST_node* question, AST_node* left, AST_node* right) :
+  AST_node(left, right) {
+    this->question = question;
+  }
+  
+  ~() {
+    delete question;
+  }
+};
+
+// for tracking type of variable declaration
+enum var_type {
+  INT, STRING, POINTER
+};
+
+class vardecl_node : public AST_node {
+ public:
+  enum var_type t;
+  bool isExtern = false;
+
+
+ vardecl_node(var_type t, AST_node* id) : AST_node(id, nullptr) {
+    this->t = t;
+  }
+
+ vardecl_node(var_type t, AST_node* id, AST_node* assign) : AST_node(id, assign) {
+    this->t = t;
+  }
+
+ vardecl_node(bool e, var_type t, AST_node* id) : AST_node(id, nullptr) {
+    isExtern = e;
+    this->t = t;
+  }
+};
+
+class function_node : AST_node {
+ public:
+  // initializes left to point to the expression to return                 
+  std::string id;
+  AST_node parameter_list; // may need to change this type
+
+ function_node(std::string id, AST_node param_list) : AST_node() {
+    this->id = id;
+    parameter_list = param_list;
+  }
+};
 
 #endif /* NODE_H */
