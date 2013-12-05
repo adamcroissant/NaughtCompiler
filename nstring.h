f5e9da8213bf26a1b495ad0eb89a6e26b717145f
Adam Croissant <adamc41@uw.edu>
2013-12-05 15:30:37 -0800

    put header guards on nstring.h

--- nstring.h
+++ nstring.h
@@ -1,6 +1,10 @@
+#ifndef __NSTRING_H__
+#define __NSTRING_H__
+
 #include <stdint.h>
 
 typedef struct nstring_st {
   int32_t    len;     // The length of the string, ignoring the trailing \0
   char       str[];   // This is a C-style string
 } nstring_st;
+#endif
