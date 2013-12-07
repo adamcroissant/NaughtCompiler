12bce7a85fa606a4ed4e6e86ad6ce9b837cdca1c
Adam Croissant <adamc41@uw.edu>
2013-12-06 22:27:32 -0800

    C function for concatenating two naught strings in the c file

---
+++ string_add.h
@@ -0,0 +1,10 @@
+#ifndef __STRING_ADD_H__
+#define __STRING_ADD_H__
+
+#include <stdlib.h>
+
+// assumes left and right are naught strings with a 
+// preceding length field
+char* string_add(char* left, char* right);
+
+#endif /* __STRING_ADD_H__ */
